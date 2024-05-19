93eb362409db0a3c8efa035e95ccb17e3905378f
hassanAdamineJad <adaminejadh@gmail.com>
2024-05-19 11:49:45 +0300

    feat: added a new component as top-employees

--- src/components/tiles.tsx
+++ src/components/tiles.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export function Tiles() {
---
+++ src/components/top-employees.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+export interface ITopEmployees {
+  current_score: number;
+  email: string;
+  name: string;
+  title: string;
+}
+
+export function TopEmployees({ data }: { data: ITopEmployees[] }) {
+  return (
+    <div className="space-y-8">
+      {data?.map((item, key) => {
+        return (
+          <div className="flex items-center" key={key}>
+            <Avatar className="h-9 w-9">
+              <AvatarFallback>
+                {item.name
+                  .match(/(\b\S)?/g)
+                  .join("")
+                  .match(/(^\S|\S$)?/g)
+                  .join("")
+                  .toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">{item.title}</p>
+              <p className="text-sm text-muted-foreground">
+                {item.name}(<span className="text-xs">{item.email}</span>){" "}
+              </p>
+            </div>
+            <div className="ml-auto font-medium">{item.current_score}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
